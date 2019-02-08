d41c0d53a8be4c179ccc9d3df0e23606a9581f04
nezz <imtaehyun@me.com>
2019-02-08 09:43:11 +0900

    주문시 상품 구매 가능 상태인지 확인

--- Scripts/ssg_order/Script1548810048132.groovy
+++ Scripts/ssg_order/Script1548810048132.groovy
@@ -14,49 +14,55 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
 '상품 페이지 이동'
-WebUI.navigateToUrl('http://qa-emart.ssg.com/item/itemView.ssg?itemId=0000008026713&siteNo=6001&salestrNo=2034')
+WebUI.navigateToUrl('http://qa-emart.ssg.com/item/itemView.ssg?itemId=${itemId}&siteNo=6001&salestrNo=2034')
 
-'바로구매 버튼 클릭'
-WebUI.click(findTestObject('Object Repository/order/a__1'))
+isValidToBuy = WebUI.verifyElementPresent(findTestObject('order/a__1'), 1)
 
-isDuhagee = WebUI.verifyElementVisible(findTestObject('order2/jumun_duhagee'))
+if (isValidToBuy) {
+    '바로구매 버튼 클릭'
+    WebUI.click(findTestObject('Object Repository/order/a__1'))
 
-'최초주문일때'
-if (isDuhagee == false) {
-    WebUI.click(findTestObject('Object Repository/order/td_800g ()'))
+    isDuhagee = WebUI.verifyElementVisible(findTestObject('order2/jumun_duhagee'))
+
+    '최초주문일때'
+    if (isDuhagee == false) {
+        WebUI.click(findTestObject('Object Repository/order/td_800g ()'))
+    } else {
+        '주문더하기가 뜰때 새로 주문하기 버튼 클릭'
+        WebUI.click(findTestObject('order2/new_order_button'))
+    }
+    
+    '쓱배송일자 선택'
+    WebUI.click(findTestObject('order/input_select_day_time_css'))
+
+    '품절상품 대체여부 > 전체수락'
+    WebUI.click(findTestObject('Object Repository/order/input_ _shortgProcMthdCdAll_1'))
+
+    WebUI.delay(3)
+
+    '계속하기 버튼 클릭'
+    WebUI.click(findTestObject('Object Repository/order/button__1'))
+
+    's포켓 전체사용 버튼 클릭'
+    WebUI.click(findTestObject('Object Repository/order/button__2'))
+
+    '"주문 상품정보 및 결제대행 서비스 이용약관에 모두 동의하십니까?" 클릭'
+    WebUI.click(findTestObject('Object Repository/order/label_'))
+
+    '결제하기 버튼 클릭'
+    WebUI.click(findTestObject('Object Repository/order/button_0  0'))
+
+    ord_no = WebUI.getText(findTestObject('order/ord_no_txt'))
+
+    ord_no = ord_no.replace('주문번호는 ', '')
+
+    ord_no = ord_no.replace('번', '')
+
+    '주문번호 Global 변수에 저장\t'
+    GlobalVariable.ord_no = ord_no
+
+    System.out.println(ord_no)
 } else {
-    '주문더하기가 뜰때 새로 주문하기 버튼 클릭'
-    WebUI.click(findTestObject('order2/new_order_button'))
+    System.out.println('상품 상태 확인 필요')
 }
-
-'쓱배송일자 선택'
-WebUI.click(findTestObject('order/input_select_day_time_css'))
-
-'품절상품 대체여부 > 전체수락'
-WebUI.click(findTestObject('Object Repository/order/input_ _shortgProcMthdCdAll_1'))
-
-WebUI.delay(3)
-
-'계속하기 버튼 클릭'
-WebUI.click(findTestObject('Object Repository/order/button__1'))
-
-'s포켓 전체사용 버튼 클릭'
-WebUI.click(findTestObject('Object Repository/order/button__2'))
-
-'"주문 상품정보 및 결제대행 서비스 이용약관에 모두 동의하십니까?" 클릭'
-WebUI.click(findTestObject('Object Repository/order/label_'))
-
-'결제하기 버튼 클릭'
-WebUI.click(findTestObject('Object Repository/order/button_0  0'))
-
-ord_no = WebUI.getText(findTestObject('order/ord_no_txt'))
-
-ord_no = ord_no.replace('주문번호는 ', '')
-
-ord_no = ord_no.replace('번', '')
-
-'주문번호 Global 변수에 저장\t'
-GlobalVariable.ord_no = ord_no
-
-System.out.println(ord_no)
 
